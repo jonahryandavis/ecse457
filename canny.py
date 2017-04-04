722f6bff8419fc277dcb6bdde8d4c07708b9690b
Katie <katherine.duff@mail.mcgill.ca>
2017-04-03 23:15:49 -0400

    Added basic canny edge detection, comparison of edges and script to remove image background

--- canny.py
+++ canny.py
@@ -1,9 +1,33 @@
-''' Basic canny edge detection with some parameters that could be adjusted by 
-machine learning '''
+""" Basic canny edge detection with some parameters that could be adjusted by 
+machine learning """
 
 import cv2
+import os
+import sys 
 import numpy as np 
+from matplotlib import pyplot as plt
 
-def Canny(img)
-	detected_edges = cv2.GaussianBlur(img, (),)
-	cv2.Canny(img, detected_edges, lowThreshold, lowThreshold*ratio, apertureSize = ap_size, True)
+def canny(img, lowThreshold, ratio, ap_size):
+	#use canny filter with passed parameters 
+	detected_edges = cv2.Canny(img, lowThreshold, lowThreshold*ratio)
+	return detected_edges
+
+def main(): 
+	img = cv2.imread('20_masked.jpg')
+	detected_edges = canny(img, 1, 200, 3)
+	cv2.imshow('img', detected_edges)                                   # Display
+	cv2.waitKey(0)
+	detected_edges_stack = np.dstack([detected_edges]*3)    # Create 3-channel alpha mask
+	#-- Blend masked img into MASK_COLOR background --------------------------------------
+	detected_edges_stack  = detected_edges_stack.astype('float32') / 255.0          # Use float matrices
+	detected_edges_stack = (detected_edges_stack * 255).astype('uint8')             # Convert back to 8-bit 
+	img_out = cv2.cvtColor(detected_edges_stack,cv2.COLOR_BGR2GRAY)  # Convert back to 8-bit 
+
+	cv2.imshow('img', img_out)                                   # Display
+	cv2.waitKey(0)
+	cv2.imwrite('20_canny.jpg',img_out)  
+	return 0	
+
+if __name__ == "__main__":
+	sys.exit(main())
+
--- compEdges.py
+++ compEdges.py
@@ -1,80 +1,112 @@
 import math
 import Image 
-import Levenshtein 
+import cv2
+import sys 
+import numpy as np
 
-''' Compares the Canny generated edge map to the training set edge map image using the following parameters imgt is the training image, imgc is the canny edge detection generated image'''
+''' Compares the Canny generated edge map to the training set edge map image using the following parameters imgA is part of the training image, imgB is part of the canny edge detection generated image'''
 
 class ImCompare(object):
 
-_pixel = 255
-_colour = False
+	_pixel = 255
 
-def __init__(self, imgt, imgc)
-'''Saves a copy of the image objects'''
-
-sizet, sizec = imgt.size, imgc.size
-
-#rescale to a common size if not already the case 
-if(sizet not= sizec):
-	imgc = imgc.resize((sizet[0],sizet[1]), Image.BICUBIC)
+def mse(imA, imB):
+# Computes the Mean Square Error between the two edge images. 
+# mse is the sum of the squared difference between the two edge images
+# both images must have the same dimensions
+	err = np.sum((imA.astype("float") - imB.astype("float"))**2)
+	err /= float(imA.shape[0]*imB.shape[1])
+#returns the MSE, the lower the value the more similar 
+	return err
 	
-#store image size 
-self.x, self.y = sizet[0],sizet[1]
+def dice(imA, imB):
+#Computes the dice value between the two edge images,  
+	a = 0 #number of pixels of value '1' in both binary images 
+	b = 0 #number of pixels taking value '1' in image B only
+	c = 0 #number of pixels taking value '1' in image A only
+	d = 0 #number of pixels taking value '0' in both binary images 
+	_pixel = 255
+	(n,m) = imA.shape
+	for i in range(0,n):
+		for j in range(0,m):
+			if(imB[i][j] != 0):
+				print(imB[i][j])
+			if(imA[i][j] == _pixel): 
+				if(imB[i][j] == _pixel):
+					a = a+1
+				else:
+					c = c+1
+			else:
+				if(imB[i][j] == 0):
+					d = d+1
+				else:
+					b = b+1
+	#if images overlap perfectly then Dice = 1, if not overlap then Dice = 0
+	dice = (2*a)/(2*a + b - c)
+	return dice
 
-def _img_int(self, img):
-'''Convert an image to a list of pixels '''
-	x, y = img.size 
-	for i in xrange(x):
-		for j in xrange(y):
-			yield img.hetpixel((i,j))
+def ssim(imA, imB):
+	_pixel = 255
+#computes structural similarity index between two images
+	if(_pixel == 1):
+		L = 0
+	else:	
+		L = _pixel 
+	K1 = 0.01
+	K2 = 0.03 
+	C1 = K1*L*K1*L
+	C2 = K2*L*K2*L
 
-@property
-def imgt_int(self):
-''' returns tuple representing training image '''
+	(n,m) = imA.shape
+	ua = 0
+	ub = 0
+	siga = 0
+	sigb = 0 
+	sigab = 0 
+	#calculate mean of both images 
+	for i in range(0,n):
+		for j in range(0,m):
+			ua = ua + imA[i][j]
+			ub = ub + imB[i][j]
+	ua = ua/(n*m)
+	ub = ub/(n*m)
+	
+	#calculate sigma values 
+	for i in range(0,n):
+		for j in range(0,m):
+			ai = imA[i][j]
+			bi = imB[i][j]
+			siga = siga + (ai-ua)*(ai-ua)
+			sigb = sigb + (bi-ub)*(bi-ub)
+			sigab = sigab + (ai-ua)*(bi-ub)
+	
+	siga = siga/(n*m)
+	sigb = sigb/(n*m)
+	sigab = sigab/(n*m)
 
-	if not hasattr(self, '_imgt_int'):
-		self._imgt_int = tuple(self._img_int(self._imgt))
+	#use computed values to calculate ssim 
+	ssim = (2*ua*ub + C1)*(2*sigab + C2)/((ua*ua - ub*ub + C1)*(ua*ua + ub*ub + C2))
+	return ssim 
+	
+			
+if __name__ == '__main__':
+	imgA = cv2.imread('test_out20.jpg')
+	imgB = cv2.imread('20_canny.jpg')
+	imgA = cv2.cvtColor(imgA,cv2.COLOR_BGR2GRAY)
+	imgB = cv2.cvtColor(imgB,cv2.COLOR_BGR2GRAY)
+	cv2.imshow('img', imgA)                                   # Display
+	cv2.waitKey(0)
+	cv2.imshow('img', imgB)                                   # Display
+	cv2.waitKey(0)
+	mse = mse(imgA, imgB)
+	print(mse)
+	dice = dice(imgA, imgB)
+	print(dice)
+	ssim = ssim(imgA, imgB)
+	print(ssim)
+	
 
-	return self._imgt_int
-
-@property 
-def imgc_int(self):
-''' returns tuple representing canny edge detection image ''' 
- 	if not hasattr(self, '_imgc_int'):
-		self._imgt_int = tuple(self._img_int(self._imgc))
-
-	return self._imgc_int
-
-@property 
-def mse(self):
-''' returns the mean square error between the two images.'''
-	if not hasattr(self, '_mse'):
-		temp = sum((a-b)**2 for a,b in zip(self.imgt_int, self.imgc_int))
-		self._mse = float(temp)/self.x/self.y
-	return self._mse
-
-@property 
-def psnr(self):
-''' calculates the peak signal-to-noise ratio. '''
-	if not hasattr(self,'_psnr'):
-		self._psnr = 20*math.log(self._pixel/math.sqrt(self.mse), 10)
-	return self._psnr
-
-
-
-
-
-ct = 0
-cf = 0
-[m n] = size()
-
-for i=1:m
-	for j=1:n
-		if ti(i,j)~=0 && ci(i,j)~=0
-			ct=ct+1
-		end
-		if ti(i,j)~=0 && ci(i,j)==0 || ti(i,j)==0 && ci(i,j)~=0)
-			cf=cf+1
-		end
-	end
-end
+	
+			
+	
+	
